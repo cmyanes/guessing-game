e832005dbc808ccd894bd07a5071db4784ddfd7c
cmyanes <cmelyanes@gmail.com>
2020-12-02 20:35:02 +0100

    guessinggame.sh modified

--- guessinggame.sh
+++ guessinggame.sh
@@ -2,63 +2,56 @@
 # File: guessinggame.sh
 
 # checks if the argument is a natural number
-# returns 0 if it's a number; 1 otherwise
+# returns true if it is a natural number; false otherwise
+function isnumber {
+	if [[ $1 =~ ^[0-9]+$ ]]
+	then
+		echo true
+	else
+		echo false
+	fi
+}
 
+# compares two numbers (arguments)
+# if $1=$2 returns 0; if $1>$2 returns 1; if $1<$2 returns -1
+function compare {
+	if [[ $1 -eq $2 ]]
+	then
+		echo 0
+	elif [[ $1 -gt $2 ]]
+	then
+		echo 1
+	else
+		echo -1
+	fi
+}
 
 while  true 
 do
 	while  true
 	do
 		read -p "How many files are in the current directory? " n
-
-		# check if the argument is a natural number
-		if [[ $n =~ ^[0-9]+$ ]]
+		if ! $(isnumber $n)
 		then
-			break
+                        echo Incorrect value. You should enter a natural number
 		else
-			echo Incorrect value. You should enter a natural number
-			echo ""
+			break
 		fi
 	done
 
 	nfiles=$(ls -1 $PWD | wc -l)
+	
+	comp=$(compare $n $nfiles)
 
-	if [[ $n -eq $nfiles ]]
-	then
-		echo Congratulations! You guessed it!
-		break
-	elif [[ $n -gt $nfiles ]]
+	if [[ $comp -gt 0 ]]
 	then
 		echo Your guess was too high
-	else
+	elif [[ $comp -lt 0 ]]
+	then
 		echo Your guess was too low
+	else
+		echo Congratulations! You guessed it!
+		break
 	fi
 	echo Try again!
-	echo ""
 done
-
-
-
-
-
-
-
-
-
-			
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
